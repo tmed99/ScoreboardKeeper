ff2ae0b0ea0dd65566b820b5de179010223cb9ec
Kevin Buffardi <kbuffardi@csuchico.edu>
2018-04-09 13:47:54 -0700

    Starter

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int MAX_PERIODS = 10;
+const int MAX_TEAMS = 4;
+const int MIN_PERIODS = 1;
+const int MIN_TEAMS = 1;
+
+int main()
+{
+  int periods;
+  int teams;
+
+  cout<<"How many competitors? ";
+  cin>>teams;
+  cout<<"How many scoring periods? ";
+  cin>>periods;
+
+  if( teams < MIN_TEAMS || periods < MIN_PERIODS || 
+      teams > MAX_TEAMS || periods > MAX_PERIODS )
+  {
+    cout<<"Must have between "<<MIN_TEAMS<<" and "<<MAX_TEAMS<<" competitors.\n";
+    cout<<"and between "<<MIN_PERIODS<<" and "<<MAX_PERIODS<<" periods.\n";
+    return 0;
+  }
+  else
+  {
+   //make scoreboard and fill it with zeros
+  
+   //once created, display the scoreboard
+  }
+  return 0;
+}
